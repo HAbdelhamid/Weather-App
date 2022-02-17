85578ac035fb52ec7974060c05e38e9ec3318d15
root <root@LAPTOP-PH7Q9GNU.localdomain>
2022-02-17 04:58:22 +0100

    Adding dynamic background image and event listners for search bar also for search button finally hide application content till the requested informations are collected from the api

--- script.js
+++ script.js
@@ -17,8 +17,24 @@ let weather = {
         $(".description").text(description);
         $(".humidity").text("humidity : " + humidity +"%");
         $(".wind").text("Wind speed : " + speed +" km/h");
+        $(".Weather").removeClass("loading");
+        $('body').css('background-image', 'url("https://source.unsplash.com/1600x900/?' + name +' ")');
+    },
+    search: function () {
+        this.fetchWeather($(".search-bar").val());
     }
 };
 
- 
+$(".search button").click(function () {
+    weather.search();
+})
+
+.keyup()
+$(".search-bar").keyup(function (event) {
+    if (event.key == "Enter"){
+        weather.search();
+    }
+})
+
+weather.fetchWeather("denver");
     
