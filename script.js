74a538edf154ba4421ab2008d2d149eef33c6c55
root <root@LAPTOP-PH7Q9GNU.localdomain>
2022-02-17 02:51:32 +0100

    Display results collected from api

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ let weather = {
     fetchWeather : function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid="+ this.apikey +"")
         .then((Response) => Response.json())
-        .then((data) => this.displayWeather(data))
+        .then((data) => this.displayWeather(data));
     },
-    
-
+    displayWeather: function (data) {
+        const { name } = data;
+        const { description, icon } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name , description, icon, temp, humidity, speed )
+        $("h2").text("Weather in " + name);
+        $(".temp").text(temp + " °C");
+        $(".icon").attr("src", `https://openweathermap.org/img/wn/${icon}.png`);
+        $(".description").text(description);
+        $(".humidity").text("humidity : " + humidity +"%");
+        $(".wind").text("Wind speed : " + speed +" km/h");
+    }
 };
+
+ 
+    
